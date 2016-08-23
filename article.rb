99e63dcdf4eefd532058aa9a6c0a6fc85204f897
Paweł Baraniewicz <pawelbaraniewicz@gmail.com>
2016-08-23 21:50:11 +0200

    refactored article to_s method

--- article.rb
+++ article.rb
@@ -11,20 +11,10 @@ class Article
   end
   
   def to_s
-    output = [title]
-    if paragraphs.empty? && sections.empty?
-      output << " #{body}\n"
-    elsif !paragraphs.empty?
-      output << " #{body}\n"
-      paragraphs.each do |paragraph|
-        output << paragraph.to_s
-      end
-    elsif !sections.empty?
-      output << " #{body}\n"
-      sections.each do |section|
-        output << section.to_s
-      end
-    end
-    output.join
+    output = []
+    output << [title, body].join(' ')
+    output += sections
+    output += paragraphs
+    output.join("\n")
   end
 end
